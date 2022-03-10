06b45d8d2423a461a1b5d8ded769e6b09c2e253c
Daisuke Sato <daisukes@cmu.edu>
2022-03-10 13:21:26 -0500

    change calibration_params behavior - previous: run calibration mode if calibration_params does not exists - current: show warning about calibration_params            _run_imu_calibration:=1 runs calibration mode

--- cabot-arduino.ino
+++ cabot-arduino.ino
@@ -96,15 +96,9 @@ void setup()
   while(!nh.connected()) {nh.spinOnce();}
   nh.loginfo("Connected");
 
-  int calibration_params[22];
-  if (!nh.getParam("~calibration_params", calibration_params, 22, 500)) {
-    nh.logerror("clibration_params is needed to use correctly.");
-    nh.logerror("You can check calibration value with /calibration topic.");
-    nh.logerror("First 22 byte is calibration data, following 4 byte is calibration status for");
-    nh.logerror("System, Gyro, Accel, Magnet, 0 (not configured) <-> 3 (configured)");
-    nh.logerror("Specify like calibration_params:=[0, 0, 0, 0 ...]");
-    nh.logerror("Visit the following link to check how to calibrate sensoe");
-    nh.logerror("https://learn.adafruit.com/adafruit-bno055-absolute-orientation-sensor/device-calibration");
+  int run_imu_calibration = 0;
+  nh.getParam("~run_imu_calibration", &run_imu_calibration, 1, 500);
+  if (run_imu_calibration != 0) {
     imuReader.calibration();
     timer.every(100, [](){
       imuReader.update();
@@ -113,11 +107,24 @@ void setup()
     nh.loginfo("Calibration Mode started");
     return;
   }
-  uint8_t offsets[22];
-  for(int i = 0; i < 22; i++) {
-    offsets[i] = calibration_params[i] & 0xFF;
-  }
 
+  int calibration_params[22];
+  uint8_t *offsets = NULL;
+  if (nh.getParam("~calibration_params", calibration_params, 22, 500)) {
+    offsets = malloc(sizeof(uint8_t) * 22);
+    for(int i = 0; i < 22; i++) {
+      offsets[i] = calibration_params[i] & 0xFF;
+    }
+  } else {
+    nh.logwarn("clibration_params is needed to use IMU (BNO055) correctly.");
+    nh.logwarn("You can run calibration by setting _run_imu_calibration:=1");
+    nh.logwarn("You can check calibration value with /calibration topic.");
+    nh.logwarn("First 22 byte is calibration data, following 4 byte is calibration status for");
+    nh.logwarn("System, Gyro, Accel, Magnet, 0 (not configured) <-> 3 (configured)");
+    nh.logwarn("Specify like calibration_params:=[0, 0, 0, 0 ...]");
+    nh.logwarn("Visit the following link to check how to calibrate sensoe");
+    nh.logwarn("https://learn.adafruit.com/adafruit-bno055-absolute-orientation-sensor/device-calibration");
+  }
 
   int touch_params[3];
   int touch_baseline;
